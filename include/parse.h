357f776601da85561aa6819218e70fcc6e8db185
Danielx2003 <ahipsterwalrus4@gnail.com>
2026-03-28 21:50:40 +0000

    feat(parser): create ast

--- include/parse.h
+++ include/parse.h
@@ -2,45 +2,77 @@
 
 #include "lexer.h" // Migrate tokens to own header file
 
-/*
+enum ast_node_type {
+  AST_NODE_TYPE_OPERATOR,
+  AST_NODE_TYPE_ID,
+  AST_NODE_TYPE_CONSTANT
+};
 
-peek_token
-consume_token
+enum ast_expr_prime_type {
+  AST_EXPR_PRIME_TYPE_EMPTY,
+  AST_EXPR_PRIME_TYPE_EXPR
+};
 
-parse_xyz - should be priv - not in header
+enum ast_operator_type {
+  AST_OPERATOR_TYPE_ADD,
+  AST_OPERATOR_TYPE_SUBTRACT
+};
 
-do {
-  peek_token()
-  consume_token()
-}
+struct ast_operator {
+  enum ast_operator_type type;
+};
 
-while (token != end_of_file)
 
-consume_token(token)
-{
-  switch(token->type)
-  {
-    case RETURN:
-      parse_return();
-      break;
-    case IDENTIFIER:
-      parse_id();
-      break;
-    default:
-      printf("Not a known token\n");
-      break;
-  }
-  tokens->idx++;
-}
+enum ast_term_type {
+  AST_TERM_TYPE_ID,
+  AST_TERM_TYPE_CONSTANT
+};
 
-peek_token(tokens_list)
-{
-  return tokens_list[tokens->idx];
-}
-*/
+// Maybe merge id and constant and differentiate by type enum
+struct ast_id {
+  char text[32];
+  size_t text_len;
+};
+
+struct ast_constant {
+  char text[32];
+  size_t text_len;
+};
+
+struct ast_term {
+  enum ast_term_type type;
+  union {
+    struct ast_id id;
+    struct ast_constant constant;
+  };
+};
+
+struct ast_expr_prime {
+  // Could be worth converting the grammar for op, term, expr' to be a single non-terminal
+  enum ast_expr_prime_type type; // Could remove and use NULL for expr_prime_ptr
+  struct ast_operator op; // Could migrate to enum, however for future changes struct is better
+  struct ast_term term;
+  struct ast_expr_prime *expr_prime;
+};
+
+struct ast_expr {
+  struct ast_term term;
+  struct ast_expr_prime expr_prime;
+};
+
+struct ast_line {
+  struct ast_expr expr;
+  // Don't see a need for terminator here
+};
+
+struct ast_root {
+  struct ast_line *lines;
+  size_t num_lines;
+};
 
 void parse_lexer_tokens(
-  struct token_list_t *lexer_output
+  struct token_list_t *lexer_output,
+  int num_lines
 );
 
 void peek_token(
