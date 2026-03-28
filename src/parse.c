22f1676b0d17a0eeb06fab79bcbcfbe373302f5d
Danielx2003 <ahipsterwalrus4@gnail.com>
2026-03-28 19:43:57 +0000

    fix(parser): error handling via panic-mode

--- src/parse.c
+++ src/parse.c
@@ -3,63 +3,113 @@
 #include <stdio.h>
 #include <string.h>
 
-static void parse_expression(struct token_list_t *lexer_output);
+static struct token_t cur_token = {0};
 
-static void parse_return(struct token_list_t *lexer_output)
+void parse_terminator(struct token_list_t *lexer_output)
 {
-  lexer_output->cur_idx += 1;
-  parse_expression(lexer_output);
-}
-
-static void parse_expression(struct token_list_t *lexer_output)
-{
-  struct token_t cur_token;
-  peek_token(lexer_output, &cur_token);
-
-  switch (cur_token.type)
+  // Anchor Set:
+  // TOKEN_TYPE_SEMI_COLON
+  
+  if (cur_token.type == TOKEN_TYPE_SEMI_COLON)
   {
-    case TOKEN_TYPE_CONSTANT:
-    case TOKEN_TYPE_ID:
-      return;
-    default:
-      printf("invalid syntax: not an expression\n");
-  } 
+    printf("%s\n", cur_token.text);
+    consume_token(lexer_output);
+    return;
+  }
+
+  printf("Expecting semi colon. Received %d\n", cur_token.type);
+  while (
+      cur_token.type != TOKEN_TYPE_ID
+      && cur_token.type != TOKEN_TYPE_CONSTANT
+      && cur_token.type != TOKEN_TYPE_EOF
+      )
+  {
+    consume_token(lexer_output);
+  }
 }
 
-void parse_lexer_tokens(struct token_list_t *lexer_output)
+void parse_term(
+    struct token_list_t *lexer_output
+)
+{
+  // Anchor Set:
+  // TOKEN_TYPE_ADD, TOKEN_TYPE_SEMI_COLON -> we used id and constatnt before...
+
+  if (cur_token.type == TOKEN_TYPE_ID
+      || cur_token.type == TOKEN_TYPE_CONSTANT)
+  {
+    printf("%s\n", cur_token.text); 
+    consume_token(lexer_output);
+    return;
+  }
+
+  printf("Error: Expecting ID or CONSTANT . Received %d\n", cur_token.type);
+  while (cur_token.type != TOKEN_TYPE_ADD
+      && cur_token.type != TOKEN_TYPE_SEMI_COLON)
+  {
+    consume_token(lexer_output);
+  }
+}
+
+void parse_expr_prime(
+    struct token_list_t *lexer_output
+)
+{
+  if (cur_token.type == TOKEN_TYPE_ADD) // change to be any arithmetic later
+  {
+    printf("%s\n", cur_token.text); 
+    consume_token(lexer_output);
+    parse_term(lexer_output);
+    parse_expr_prime(lexer_output);
+  }
+}
+
+void parse_expr(
+    struct token_list_t *lexer_output
+)
+{
+  parse_term(lexer_output);
+  parse_expr_prime(lexer_output);
+}
+
+void parse_line(
+    struct token_list_t *lexer_output
+)
+{
+  parse_expr(lexer_output);
+  parse_terminator(lexer_output);
+}
+
+void parse_lexer_tokens(struct token_list_t *lexer_output) // Equivalent to parse_program
 {
   // Using the cur_idx produces side effects -> consider a different method later
   
-  lexer_output->cur_idx = 0;
-  
-  struct token_t token;
+  lexer_output->cur_idx = 0; 
+
   do {
-    peek_token(lexer_output, &token);
-    consume_token(lexer_output, &token);
-  } while (token.type != TOKEN_TYPE_EOF);
+    peek_token(lexer_output);
+
+    if (cur_token.type == TOKEN_TYPE_EOF) { printf("at EOF"); continue; }
+    parse_line(lexer_output);
+
+  } while (cur_token.type != TOKEN_TYPE_EOF && lexer_output->cur_idx < lexer_output->total_tokens);
+  
+  printf("--- Done ---\n");
 }
 
 void peek_token(
-    struct token_list_t *lexer_output,
-    struct token_t *token
+    struct token_list_t *lexer_output
 )
 {
-  memcpy(token, &lexer_output->tokens[lexer_output->cur_idx], sizeof(struct token_t));
+  memcpy(&cur_token, &lexer_output->tokens[lexer_output->cur_idx], sizeof(struct token_t));
 }
 
 void consume_token(
-    struct token_list_t *lexer_output,
-    struct token_t *token
+    struct token_list_t *lexer_output
 )
 {
-  switch (token->type)
-  {
-    case TOKEN_TYPE_RETURN:
-      parse_return(lexer_output);      
-      break;
-    default:
-      break;
-  }
+  if (lexer_output->cur_idx > lexer_output->total_tokens) { return; }
 
   lexer_output->cur_idx++;
+  peek_token(lexer_output);
 }
