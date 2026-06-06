2d08f14dcedad19ffd3fa8146970d149da1b8058
Dan <70431670+Danielx2003@users.noreply.github.com>
2026-06-06 14:57:05 +0100

    ref(codegen): moved instruction creation to function (#13)

--- src/codegen.c
+++ src/codegen.c
@@ -1,4 +1,5 @@
 #include "codegen.h"
+#include "asm_types.h"
 
 #include <stdio.h>
 #include <string.h>
@@ -7,6 +8,59 @@
 int offset = 8;
 int OFFSETS[256];
 FILE *file;
+
+
+char *ir_term_to_register(struct ir_term *term)
+{
+  static char buf[32];
+
+  if (term->type == IR_TERM_ID)
+  {
+    sprintf(buf, "[rsp-%d]", OFFSETS[term->text[0]]);
+  }
+  else if (term->type == IR_TERM_TEMP)
+  {
+    sprintf(buf, "[rsp-%d]", OFFSETS[term->temp]);
+  }
+  else if (term->type == IR_TERM_CONSTANT)
+  {
+    sprintf(buf, "%d", term->constant);
+  }
+
+  return buf;
+}
+
+void try_register_term(struct ir_term *term)
+{
+  if (term->type == IR_TERM_TEMP)
+  {
+    if (OFFSETS[term->temp] == 0)
+    {
+      printf("set: t%d = %d\n", term->temp, offset);
+      OFFSETS[term->temp] = offset;
+      offset += 8;
+    }
+  }
+  else if (term->type == IR_TERM_ID)
+  {
+    if (OFFSETS[term->text[0]] == 0)
+    {
+      printf("set: %s = %d\n", term->text, offset);
+      OFFSETS[term->text[0]] = offset;
+      offset += 8;
+    }
+  }
+}
+
+void create_instruction_register_term(enum asm_instr instr, enum asm_register lhs, struct ir_term *rhs)
+{
+  fprintf(file, "%s %s, %s\n", asm_instr_to_text(instr), asm_register_to_text(lhs), ir_term_to_register(rhs));
+}
+
+void create_instruction_term_register(enum asm_instr instr, struct ir_term *lhs, enum asm_register rhs)
+{
+  fprintf(file, "%s %s, %s\n", asm_instr_to_text(instr), ir_term_to_register(lhs), asm_register_to_text(rhs));
+}
 
 void generate_goto(struct ir_goto *go_to)
 {
@@ -25,132 +79,24 @@ void generate_label(struct ir_label *label)
 
 void generate_assign(struct ir_assign *assign)
 {
-  // if no rhs_2 (i.e. no operation)
   if (assign->op == IR_ASSIGN_OP_NONE)
   {
-    char buf[100];
-    if (assign->rhs_1.type == IR_TERM_ID)
-    {
-      fprintf(file, "mov rax, [rsp - %d]\n", OFFSETS[assign->rhs_1.text[0]]);
-      sprintf(buf, "rax");
-    }
-    else if (assign->rhs_1.type == IR_TERM_TEMP)
-    {
-      fprintf(file, "mov rax, [rsp - %d]\n", OFFSETS[assign->rhs_1.temp]);
-
-    }
-    else if (assign->rhs_1.type == IR_TERM_CONSTANT)
-    {
-      fprintf(file, "mov rax, %d\n", assign->rhs_1.constant);
-    }
-    
-    if (assign->lhs.type == IR_TERM_TEMP)
-    {
-      if (OFFSETS[assign->lhs.temp] == 0)
-      {
-        printf("set: t%d = %d\n", assign->lhs.temp, offset);
-        OFFSETS[assign->lhs.temp] = offset;
-        offset += 8;
-      }
-      fprintf(file, "mov [rsp - %d], rax\n", OFFSETS[assign->lhs.temp]);
-    }
-    else if (assign->lhs.type == IR_TERM_ID)
-    {
-      if (OFFSETS[assign->lhs.text[0]] == 0)
-      {
-        printf("set: %s = %d\n", assign->lhs.text, offset);
-        OFFSETS[assign->lhs.text[0]] = offset;
-        offset += 8;
-      }
-      fprintf(file, "mov [rsp - %d], rax\n", OFFSETS[assign->lhs.text[0]]);
-    }
+    create_instruction_register_term(ASM_INSTR_MOV, ASM_REGISTER_RAX, &assign->rhs_1);
+    try_register_term(&assign->lhs); // will be removed once register alloc is done
+    create_instruction_term_register(ASM_INSTR_MOV, &assign->lhs, ASM_REGISTER_RAX);
   }
   else if (assign->op == IR_ASSIGN_OP_ADD)
   {
-    if (
-      assign->rhs_2.type == IR_TERM_ID
-    )
-    {
-      fprintf(file, "mov rax, [rsp - %d]\n", OFFSETS[assign->rhs_2.text[0]]);
-    }
-    else if (assign->rhs_2.type == IR_TERM_TEMP)
-    {
-      fprintf(file, "mov rax, [rsp - %d]\n", OFFSETS[assign->rhs_2.temp]);
-    }
-    else 
-    {
-      fprintf(file, "mov rax, %d\n", assign->rhs_2.constant);
-    }
- 
-    if (
-      assign->rhs_1.type == IR_TERM_ID
-    )
-    {
-      fprintf(file, "add rax, [rsp - %d]\n", OFFSETS[assign->rhs_1.text[0]]);
-    }
-    else if (assign->rhs_1.type == IR_TERM_TEMP)
-    {
-      fprintf(file, "add rax, [rsp - %d]\n", OFFSETS[assign->rhs_1.temp]);
-    }
-    else 
-    {
-      fprintf(file, "add rax, %d\n", assign->rhs_1.constant);
-    }
+    create_instruction_register_term(ASM_INSTR_MOV, ASM_REGISTER_RAX, &assign->rhs_2);
+    create_instruction_register_term(ASM_INSTR_ADD, ASM_REGISTER_RAX, &assign->rhs_1);
 
-    if (assign->lhs.type == IR_TERM_TEMP)
-    {
-      if (OFFSETS[assign->lhs.temp] == 0)
-      {
-        printf("set: t%d = %d\n", assign->lhs.temp, offset);
-        OFFSETS[assign->lhs.temp] = offset;
-        offset += 8;
-      }
-
-      fprintf(file, "mov [rsp - %d], rax\n", OFFSETS[assign->lhs.temp]);
-    }
-    else if (assign->lhs.type == IR_TERM_ID)
-    {
-      if (OFFSETS[assign->lhs.text[0]] == 0)
-      {
-        printf("set: %s = %d\n", assign->lhs.text, offset);
-        OFFSETS[assign->lhs.text[0]] = offset;
-        offset += 8;
-      }
-      fprintf(file, "mov [rsp - %d], rax (lhs = id)\n", OFFSETS[assign->lhs.text[0]]);
-    }
-
+    try_register_term(&assign->lhs); // will be removed once register alloc is done
+    create_instruction_term_register(ASM_INSTR_MOV, &assign->lhs, ASM_REGISTER_RAX);
   }
   else if (assign->op == IR_ASSIGN_OP_EQUIV)
   {
-    if (
-      assign->rhs_2.type == IR_TERM_ID
-    )
-    {
-      fprintf(file, "mov rax, [rsp - %d]\n", OFFSETS[assign->rhs_2.text[0]]);
-    }
-    else if (assign->rhs_2.type == IR_TERM_TEMP)
-    {
-      fprintf(file, "mov rax, [rsp - %d]\n", OFFSETS[assign->rhs_2.temp]);
-    }
-    else 
-    {
-      fprintf(file, "mov rax, %d\n", assign->rhs_2.constant);
-    }
- 
-    if (
-      assign->rhs_1.type == IR_TERM_ID
-    )
-    {
-      fprintf(file, "cmp rax, [rsp - %d]\n", OFFSETS[assign->rhs_1.text[0]]);
-    }
-    else if (assign->rhs_1.type == IR_TERM_TEMP)
-    {
-      fprintf(file, "cmp rax, [rsp - %d] (rhs_1.temp)\n", OFFSETS[assign->rhs_1.temp]);
-    }
-    else 
-    {
-      fprintf(file, "cmp rax, %d\n", assign->rhs_1.constant);
-    }
+    create_instruction_register_term(ASM_INSTR_MOV, ASM_REGISTER_RAX, &assign->rhs_2);
+    create_instruction_register_term(ASM_INSTR_CMP, ASM_REGISTER_RAX, &assign->rhs_1);
   }
   
 }
